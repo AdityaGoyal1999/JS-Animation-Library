345d75f81a615546a5d1e497bbd009dffd98c355
Aditya Goyal <adityagoyaldoon@gmail.com>
2020-12-25 10:42:48 +0530

    First effect in motion

--- pub/examples.js
+++ pub/examples.js
@@ -2,7 +2,8 @@
 addEventListener("DOMContentLoaded", ()=>{
 	const bg = new BackgroundGenerator();
 	bg.addSimpleBackground();
-
+	bg.rippleText();
+	// bg.rippleButton();
 	// bg.addBubbles();
 	// bg.iconsBackground();
 });
--- pub/library.js
+++ pub/library.js
@@ -11,4 +11,46 @@ BackgroundGenerator.prototype = {
 		const bg = document.querySelector("#background-layer-1");
 		bg.style = "background-color: bisque";
 	},
+
+	rippleText: function(){
+		const texts = document.querySelectorAll(".ripple-text");
+		texts.forEach((text)=>{
+			const content = text.innerHTML.trim();
+			text.innerText = "";
+			
+			// Loop over each letter and assign span to all letters
+			for(let i = 0; i<content.length;i++){
+				let createdSpan = document.createElement("span");
+				createdSpan.innerHTML = content[i];
+				createdSpan.className = "ripple-text-span";
+				text.append(createdSpan);
+			}
+		});
+		const spans = document.querySelectorAll(".ripple-text-span");
+		spans.forEach((span)=>{
+			span.addEventListener("mouseover", ()=>{
+				console.log("over");
+				span.style="color: blue; padding: 10px; font-size: 50px;"
+				
+			});
+			span.addEventListener("mouseout", ()=>{
+				// console.log("over");
+				span.style="color: black; padding: 0px;"
+			});
+		});
+	},
+
+	// rippleButton: function(){
+	// 	const bg = document.querySelector("#background-layer-2");
+	// 	// bg.style="background-color: black;"
+	// 	const buttons = document.querySelectorAll(".ripple-button");
+	// 	buttons.forEach((button)=>{
+	// 		const textContent = button.innerHTML;
+	// 		button.innerHTML = "";
+	// 		for(let i = 0; i<50; i++){
+	// 			let span = document.createElement("span");
+	// 			button.append(span);
+	// 		}
+	// 	});
+	// },
 }
